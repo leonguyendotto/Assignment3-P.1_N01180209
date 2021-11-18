63fdf76072fa5babe75de06715d87347393a3bb5
leonguyendotto <leonguyen.to@MSI>
2021-11-17 21:26:33 -0500

    Add project files.

---
+++ Assignment3_N01180209/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assignment3_N01180209.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ Assignment3_N01180209/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment3_N01180209.WebApiApplication" Language="C#" %>
