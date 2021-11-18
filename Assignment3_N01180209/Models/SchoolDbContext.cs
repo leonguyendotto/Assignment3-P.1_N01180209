2143eb4011a9e850908e8f55c6f5df58cfea2d4e
leonguyendotto <leonguyen.to@MSI>
2021-11-17 23:37:56 -0500

    Updated

---
+++ Assignment3_N01180209/Models/SchoolDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Assignment3_N01180209.Models
+{
+    public class SchoolDbContext 
+    {
+        //Only the SchoolDb class can use them.
+        //These properties are not meant to change so they are read-only
+        private static string User { get { return "root"; } }
+        private static string Password { get { return "root"; } }
+        private static string Database { get { return "school"; } }
+        private static string Server { get { return "localhost"; } }
+        private static string Port { get { return "3306"; } }
+
+        //ConnectionString is a series of credentials used to connect to eh database.
+
+        protected static string ConnectionString
+        {
+            get
+            {
+                return "server = " + Server
+                    + "; user = " + User
+                    + "; database = " + Database
+                    + "; port = " + Port
+                    + "; password = " + Password;
+            }
+        }
+        //This is the way we use to get the database 
+        /// <summary>
+        /// Returns a connection to the school database.
+        /// </summary>
+        /// <example>
+        /// MySqlConnection Conn = Blog.ACcessDatabase ()
+        /// </example>
+        /// <returns>A MySqlConnection Object</returns>
+        public MySqlConnection AccessDatabase ()
+        {
+            //We are instantiating the MySqlConnection Class to create an object
+            //the object is a specific connection to our school database on port 3306 of localhost
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
